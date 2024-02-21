46c497176fa134ff5c740f2a25d7f349b5403eb3
Stephonj <stephonj@email.sc.edu>
2024-02-21 16:18:07 -0500

    worked on #24

---
+++ AdvisingSoftware/MajorMap.java
@@ -0,0 +1,82 @@
+package AdvisingSoftware;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+/**
+ * Represents a mapping of courses for a specific major.
+ * @author Stephon Johnson
+ */
+public class MajorMap {
+    private UUID id;
+    private String major;
+    private ArrayList<Course> majorCourses;
+    private ArrayList<Course> majorElective;
+    private ArrayList<Course> coreEdu;
+    private ArrayList<Course> appArea;
+
+    /**
+     * Constructor with an ID for the MajorMap.
+     *
+     * @param id           The UUID of the MajorMap.
+     * @param major        The name of the major.
+     * @param majorCourses The courses needed for the major.
+     * @param majorElective The elective for the major.
+     * @param coreEdu      The core courses for the major.
+     * @param appArea      The courses for the major's application area.
+     */
+    public MajorMap(UUID id, String major, ArrayList<Course> majorCourses, ArrayList<Course> majorElective, ArrayList<Course> coreEdu, ArrayList<Course> appArea) {
+        this.id = id;
+        this.major = major;
+        this.majorCourses = majorCourses;
+        this.majorElective = majorElective;
+        this.coreEdu = coreEdu;
+        this.appArea = appArea;
+    }
+
+    /**
+     * Constructor without an ID for the MajorMap.
+     *
+     * @param major        The name of the major.
+     * @param majorCourses The courses needed for the major.
+     * @param majorElective The elective courses for the major.
+     * @param coreEdu      The core courses for the major.
+     * @param appArea      The courses for the major's application area.
+     */
+    public MajorMap(String major, ArrayList<Course> majorCourses, ArrayList<Course> majorElective, ArrayList<Course> coreEdu, ArrayList<Course> appArea) {
+        this.id = UUID.randomUUID();
+        this.major = major;
+        this.majorCourses = majorCourses;
+        this.majorElective = majorElective;
+        this.coreEdu = coreEdu;
+        this.appArea = appArea;
+    }
+
+    /**
+     * Deletes the major from the system.
+     *
+     * @param major The name of the major to delete.
+     */
+    public void deleteMajor(String major) {
+    }
+
+    /**
+     * Edits the major's details.
+     *
+     * @param major        The name of the major.
+     * @param majorCourses The courses required for the major.
+     * @param majorElective The elective courses for the major.
+     * @param coreEdu      The core education courses for the major.
+     * @param appArea      The courses for the major's application area.
+     */
+    public void editMajor(String major, ArrayList<Course> majorCourses, ArrayList<Course> majorElective, ArrayList<Course> coreEdu, ArrayList<Course> appArea) {
+    }
+
+    /**
+     * Displays the details of the major map.
+     *
+     * @param major The name of the major to display.
+     */
+    public void displayMajorMap(String major) {
+    }
+}
