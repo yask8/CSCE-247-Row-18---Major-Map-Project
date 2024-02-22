ff3776ba10c2bab350e2a705365575b97ad4481c
yask8 <yasminekennedy88@gmail.com>
2024-02-22 12:19:15 -0500

    fixed #27

--- AdvisingSoftware/Semester.java
+++ AdvisingSoftware/Semester.java
@@ -1,7 +1,7 @@
 package AdvisingSoftware;
 /**
  * Enum class that holds all of the semester availability options
- * @author yask8(ykennedy)
+ * @author yask8(Yasmine Kennedy)
  */
 public enum Semester {
     FALL, SPRING, SUMMER
