932924c9f048b80a476b8aae6b6a50dda05ca7df
unknown <liaz@email.sc.edu>
2024-02-22 20:57:40 -0500

    fixed typo in #48

--- AdvisingSoftware/Class.java
+++ AdvisingSoftware/Class.java
@@ -1,8 +1,12 @@
 package AdvisingSoftware;
+
 /**
  * Holds the student's classification level
  * @author yask8(Yasmine Kennedy) and oshumate(Owen Shumate)
  */
 public enum Class {
-    FRESHMAN, SOPHMORE, JUNIOR, SENIOR
+  FRESHMAN,
+  SOPHOMORE,
+  JUNIOR,
+  SENIOR,
 }
