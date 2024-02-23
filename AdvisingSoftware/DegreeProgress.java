114547b4ed00e85b904f56ed806600a4aee16a22
garrettspillman1 <garrettspillman1@gmail.com>
2024-02-23 10:21:04 -0500

    Stukck on reading in students #12

--- AdvisingSoftware/DegreeProgress.java
+++ AdvisingSoftware/DegreeProgress.java
@@ -7,18 +7,15 @@ public class DegreeProgress {
     private String major;
     private ArrayList<Course> majorCourses;
     private ArrayList<Course> electiveCourses;
-    private ArrayList<Course> minorCourses;
     private ArrayList<Course> carolinaCoreCourses;
     private ArrayList<Course> completeCourses;
     private ArrayList<Course> incompleteCourses;
 
     public DegreeProgress(String major, ArrayList<Course> majorCourses, ArrayList<Course> electiveCourses,
-                          ArrayList<Course> minorCourses, ArrayList<Course> carolinaCoreCourses,
-                          ArrayList<Course> completeCourses, ArrayList<Course> incompleteCourses) {
+                         ArrayList<Course> carolinaCoreCourses,ArrayList<Course> completeCourses, ArrayList<Course> incompleteCourses) {
         this.major = major;
         this.majorCourses = majorCourses;
         this.electiveCourses = electiveCourses;
-        this.minorCourses = minorCourses;
         this.carolinaCoreCourses = carolinaCoreCourses;
         this.completeCourses = completeCourses;
         this.incompleteCourses = incompleteCourses;
