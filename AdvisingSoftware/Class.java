76dece1bea7d2676711ec3122240c234ec1e2e9c
yask8 <yasminekennedy88@gmail.com>
2024-02-22 20:52:36 -0500

    fixed #35

---
+++ AdvisingSoftware/Class.java
@@ -0,0 +1,8 @@
+package AdvisingSoftware;
+/**
+ * Holds the student's classification level
+ * @author yask8(Yasmine Kennedy) and oshumate(Owen Shumate)
+ */
+public enum Class {
+    FRESHMAN, SOPHMORE, JUNIOR, SENIOR
+}
