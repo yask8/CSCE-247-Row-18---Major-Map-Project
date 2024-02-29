64df6df9007d3a261d9da15601822fb1cb715e7e
Stephonj <stephonj@email.sc.edu>
2024-02-29 14:24:55 -0500

    Combined GradRequirements with MajorMap

--- AdvisingSoftware/MajorMap.java
+++ AdvisingSoftware/MajorMap.java
@@ -17,6 +17,12 @@ public class MajorMap {
   private ArrayList<Course> coreEdu;
   private ArrayList<Course> appArea;
 
+  // Graduation Requirements
+  private int minTotalHours;
+  private int minGradHours;
+  private int caroCoreHours;
+  private Double minGPA;
+
   /**
    * Constructor with an ID for the MajorMap.
    *
@@ -33,13 +39,21 @@ public class MajorMap {
       ArrayList<Course> majorCourses,
       ArrayList<Course> majorElective,
       ArrayList<Course> coreEdu,
-      ArrayList<Course> appArea) {
+      ArrayList<Course> appArea,
+      int minTotalHours,
+      int minGradHours,
+      int caroCoreHours,
+      Double minGPA) {
     this.id = id;
     this.major = major;
     this.majorCourses = majorCourses;
     this.majorElective = majorElective;
     this.coreEdu = coreEdu;
     this.appArea = appArea;
+    this.minTotalHours = minTotalHours;
+    this.minGradHours = minGradHours;
+    this.caroCoreHours = caroCoreHours;
+    this.minGPA = minGPA;
   }
 
   /**
@@ -56,13 +70,21 @@ public class MajorMap {
       ArrayList<Course> majorCourses,
       ArrayList<Course> majorElective,
       ArrayList<Course> coreEdu,
-      ArrayList<Course> appArea) {
+      ArrayList<Course> appArea,
+      int minTotalHours,
+      int minGradHours,
+      int caroCoreHours,
+      Double minGPA) {
     this.id = UUID.randomUUID();
     this.major = major;
     this.majorCourses = majorCourses;
     this.majorElective = majorElective;
     this.coreEdu = coreEdu;
     this.appArea = appArea;
+    this.minTotalHours = minTotalHours;
+    this.minGradHours = minGradHours;
+    this.caroCoreHours = caroCoreHours;
+    this.minGPA = minGPA;
   }
 
   /**
@@ -78,6 +100,10 @@ public class MajorMap {
       this.coreEdu = null;
       this.appArea = null;
       this.id = null;
+      this.minTotalHours = 0;
+      this.minGradHours = 0;
+      this.caroCoreHours = 0;
+      this.minGPA = 0.0;
     }
   }
 
@@ -95,6 +121,10 @@ public class MajorMap {
       System.out.println("Major Elective: " + this.majorElective);
       System.out.println("Carolina Core: " + this.coreEdu);
       System.out.println("Application Area: " + this.appArea);
+      System.out.println("Minimum Total Hours: " + this.minTotalHours);
+      System.out.println("Minimum Grad Hours: " + this.minGradHours);
+      System.out.println("Carolina Core Hours: " + this.caroCoreHours);
+      System.out.println("Minimum GPA: " + this.minGPA);
     } else {
       System.out.println("No matching major found.");
     }
@@ -114,7 +144,11 @@ public class MajorMap {
         "\n********* Carolina Core *********\n" +
         "Carolina Core: " + this.coreEdu + "\n" +
         "\n********* Application Area *********\n" +
-        "Application Area: " + this.appArea + "\n";
+        "Application Area: " + this.appArea + "\n" +
+        "Minimum Total Hours: " + this.minTotalHours + "\n" +
+        "Minimum Grad Hours: " + this.minGradHours + "\n" +
+        "Carolina Core Hours: " + this.caroCoreHours + "\n" +
+        "Minimum GPA: " + this.minGPA + "\n";
   }
 
   /**
@@ -162,13 +196,90 @@ public class MajorMap {
     return coreEdu;
   }
 
-  /**
+    /**
    * Get the list of courses for the major's application area.
    *
    * @return The list of courses for the major's application area.
    */
   public ArrayList<Course> getAppArea() {
     return appArea;
+  }
+
+  /**
+   * Get the minimum total hours required for graduation.
+   *
+   * @return The minimum total hours required for graduation.
+   */
+  public int getMinTotalHours() {
+    return minTotalHours;
+  }
+
+  /**
+   * Get the minimum graduation hours required.
+   *
+   * @return The minimum graduation hours required.
+   */
+  public int getMinGradHours() {
+    return minGradHours;
+  }
+
+  /**
+   * Get the Carolina Core hours required.
+   *
+   * @return The Carolina Core hours required.
+   */
+  public int getCaroCoreHours() {
+    return caroCoreHours;
+  }
+
+  /**
+   * Get the minimum GPA required for graduation.
+   *
+   * @return The minimum GPA required for graduation.
+   */
+  public Double getMinGPA() {
+    return minGPA;
+  }
+
+
+  /**
+     * Sets the minimum total hours
+     * @param minTotalHr the hours given
+     */
+    public void setMinTotalHours(int minTotalHr){
+        if( minTotalHr > 0){
+            this.minTotalHours = minTotalHr;
+        }
+    }
+    
+    /**
+     * Sets the minimum graduation hours
+     * @param minGradHr the hours given
+     */
+    public void setMinGradHours(int minGradHr){
+        if(minGradHr > 0){
+            this.minGradHours = minGradHr;
+        }
+    }
+
+  /**
+     * Sets the Carolina Core Hours
+     * @param minCaroCoreHr the hours given
+     */
+    public void setCaroCoreHours(int minCaroCoreHr){
+      if(minCaroCoreHr > 0){
+          this.caroCoreHours = minCaroCoreHr;
+      }
+  }
+
+  /**
+     * Sets the minimum GPA
+     * @param minGPA the minimum GPA
+     */
+    public void setMinGPA(Double minGPA){
+      if(minGPA > 0){
+          this.minGPA = minGPA;
+      }
   }
 
   public boolean containsCourse(Course course) {
