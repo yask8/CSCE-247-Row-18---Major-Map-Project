3eeaa744c8bf2002a9c31da82dbdcd7f82f80217
yask8 <yasminekennedy88@gmail.com>
2024-03-13 23:28:45 -0400

    testing

--- AdvisingSoftware/Driver.java
+++ AdvisingSoftware/Driver.java
@@ -17,11 +17,12 @@ public class Driver {
   public void run() {
     //Testing Scenarios
     //scenarioTesting();
-    //scenarioAddingCourses();
+    scenarioAddingSem1Courses();
+    scenarioAddingSem2Courses();
 
     // Portias Scenarios
     // scenario1();
-     scenario2();
+     //scenario2();
 
   }
 
@@ -100,10 +101,30 @@ public class Driver {
 
   }
 
-  public void scenarioAddingCourses() {
-    String name = "MATH 141 Calculus 1";
+  public void scenarioAddingSem1Courses() {
+    // ENG 101
+    String ENG101 = "Intruction in strategies for critically reading and analyzing" +
+    " literature and non-literary texts; structured, sustained practice in composing" + 
+    " expository and analytical essays.";
+    ArrayList<String> re = new ArrayList<String>();
+    facade.getCourseList().addCourse("ENGL 101 Critical Reading and Composition", 
+                                      "CC-CMW", 
+                                      ENG101, 
+                                      3, 
+                                      "ENGL", 
+                                      'C', 
+                                      false, 
+                                      false, 
+                                      re, 
+                                      "1", 
+                                      "1");
+    // MATH 141
+    String name = "MATH 141 Calculus I";
     String code = "CC-ARP";
-    String description = "Functions, limits, derivatives, introduction to integrals, and the Fundamental Theorem of Calculus";
+    String description = 
+          "Functions, limits, derivatives, introduction to integrals," + 
+          " the Fundamental Theorem of Calculus, applications of derivatives " +
+          "integrals.";
     int creditHours = 4;
     String subject = "MATH";
     char passGrade = 'C';
@@ -121,6 +142,238 @@ public class Driver {
 
     facade.getCourseList().addCourse(name, code, description, creditHours, subject, passGrade, elective, carolinaCore,
         preReqs, year, semester);
+
+    // CSCE 145
+    String CSCE145 = "Problem-solving, algorithmic design, and programming.";
+    ArrayList<String> preRequire = new ArrayList<String>();
+    String preR = "MATH 111";
+    String preRe = "MATH 115";
+    preRequire.add(preR);
+    preRequire.add(preRe);
+
+    facade.getCourseList().addCourse("CSCE 145 Algorithmic Design I", 
+                                      "PR", 
+                                      CSCE145, 
+                                      4, 
+                                      "CSCE", 
+                                      'C', 
+                                      false, 
+                                      false, 
+                                      preRequire, 
+                                      "1", 
+                                      "1");
+    // CSCE 190
+    String CSCE190 = "An introduction to the field of computing; trends in" + 
+    " computing technology, the profession, and careers; subdisciplines in computing;"+
+    " the nature of research and development";
+    ArrayList<String> preRequirement = new ArrayList<String>();
+    String p = "CSCE 145";
+    String r = "CSCE 204";
+    String e = "CSCE 205";
+    String z = "CSCE 206";
+    preRequirement.add(p);
+    preRequirement.add(r);
+    preRequirement.add(e);
+    preRequirement.add(z);
+
+      facade.getCourseList().addCourse("CSCE 190 Computing in the Modern World", 
+                                      "PR", 
+                                      CSCE190, 
+                                      1, 
+                                      "CSCE", 
+                                      'C', 
+                                      false, 
+                                      false, 
+                                      preRequirement, 
+                                      "1", 
+                                      "1");
+    // Carolina Core AIU 1
+    String CCA1 = "Introduction to art appreciation. Elements and " + 
+    "principles of the visual arts, with examples from the history of art.";
+    ArrayList<String> pre = new ArrayList<String>();
+
+    facade.getCourseList().addCourse("ARTE 101 Introduction to Art", 
+                                    "CC-AIU", 
+                                    CCA1, 
+                                    3, 
+                                    "ARTE", 
+                                    'C', 
+                                    false, 
+                                    true, 
+                                    pre, 
+                                    "1", 
+                                    "1");
+    // Carolina Core AIU 2
+    String CCA2 = "Introuduction to the critical study of film and media." +
+    " Students will closely analyze moving images and develop written arguments" +
+    " about film and media.";
+    ArrayList<String> require = new ArrayList<String>();
+
+    facade.getCourseList().addCourse("FAMS 240 Film and Media Analysis", 
+                                    "CC-AIU", 
+                                    CCA2, 
+                                    3, 
+                                    "FAMS", 
+                                    'C', 
+                                    false, 
+                                    true, 
+                                    require, 
+                                    "1", 
+                                    "1");
+    facade.saveCourses();
+  }
+
+  public void scenarioAddingSem2Courses() {
+    // MATH 142
+    String MATH142 = 
+          "Methods of integration, sequences and series, approximations.";
+    ArrayList<String> preReqs = new ArrayList<String>();
+    String pre1 = "MATH 141";
+    preReqs.add(pre1);
+
+    facade.getCourseList().addCourse("MATH 142 Calculus II", 
+                                     "CC-ARP", 
+                                     MATH142, 
+                                     4, 
+                                     "MATH", 
+                                     'C', 
+                                     false, 
+                                     false,
+                                     preReqs, 
+                                     "1", 
+                                     "2");
+
+    // CHEM 111
+    String CHEM111 = "Survey of the principles that underlie all chemistry with applications" +
+    " illustrating these principles.";
+    ArrayList<String> preRequire = new ArrayList<String>();
+    String preR = "MATH 111";
+    String preRe = "MATH 115";
+    String preReq = "MATH 122";
+    String preRequ = "MATH 141";
+    preRequire.add(preR);
+    preRequire.add(preRe);
+    preRequire.add(preReq);
+    preRequire.add(preRequ);
+
+    facade.getCourseList().addCourse("CHEM 111 General Chemistry I", 
+                                      "CC-SCI", 
+                                      CHEM111, 
+                                      3, 
+                                      "CHEM", 
+                                      'C', 
+                                      false, 
+                                      false, 
+                                      preRequire, 
+                                      "1", 
+                                      "2");
+    // CHEM 111L
+    String CHEM111L = "Introduction to the principles and techniques of experimental" +
+    " chemistry with emphasis on formula investigations, equations, elementary statistics," +
+    " chemical reactivity.";
+    ArrayList<String> preRequirement = new ArrayList<String>();
+    String p = "MATH 111";
+    String r = "MATH 115";
+    preRequirement.add(p);
+    preRequirement.add(r);
+
+      facade.getCourseList().addCourse("CHEM 111L General Chemistry I Lab", 
+                                      "CC-SCI", 
+                                      CHEM111L, 
+                                      1, 
+                                      "CHEM", 
+                                      'C', 
+                                      false, 
+                                      false, 
+                                      preRequirement, 
+                                      "1", 
+                                      "2");
+    // PHYS 211
+    String PHYS211 = "Classical mechanics and wave motion. Calculus-level course"+
+    " for students of science and engineering";
+    ArrayList<String> pre = new ArrayList<String>();
+    String a = "MATH 111";
+    String b = "MATH 115";
+    String c = "MATH 122";
+    String d = "MATH 141";
+    pre.add(a);
+    pre.add(b);
+    pre.add(c);
+    pre.add(d);
+
+    facade.getCourseList().addCourse("PHYS 211 Essentials of Physics I", 
+                                    "CC-SCI", 
+                                    PHYS211, 
+                                    3, 
+                                    "PHYS", 
+                                    'C', 
+                                    false, 
+                                    false, 
+                                    pre, 
+                                    "1", 
+                                    "2");
+    // PHYS 211L
+    String PHYS211L = "Experiments, excerises, and demonstrations to accompany PHYS 211.";
+    ArrayList<String> require = new ArrayList<String>();
+    String j = "MATH 111";
+    String k = "MATH 115";
+    require.add(j);
+    require.add(k);
+
+    facade.getCourseList().addCourse("PHYS 211L Essentials of Physics I Lab", 
+                                    "CC-SCI", 
+                                    PHYS211L, 
+                                    1, 
+                                    "PHYS", 
+                                    'C', 
+                                    false, 
+                                    false, 
+                                    require, 
+                                    "1", 
+                                    "2");
+
+    // CSCE 146
+    String CSCE146 = 
+        "Continuation of CSCE 145. Rigorous development of " +
+        "algorithms and computer programs; elementary data structures.";
+    ArrayList<String> requiredpre = new ArrayList<String>();
+    String v = "CSCE 145";
+    String f = "MATH 122";
+    String s = "MATH 141";
+    requiredpre.add(v);
+    requiredpre.add(f);
+    requiredpre.add(s);
+    
+    facade.getCourseList().addCourse("CSCE 146 Algorithmic Design II", 
+                                     "PR", 
+                                     CSCE146,
+                                     4, 
+                                     "CSCE", 
+                                     'C', 
+                                     false, 
+                                     false, 
+                                     requiredpre, 
+                                     "1", 
+                                     "2");
+    // CSCE 215
+    String CSCE215 =
+        "UNIX operating system, user-level system commands," +
+        " programming tools. UNIX scripting languages.";
+    ArrayList<String> requires = new ArrayList<String>();
+    String g = "CSCE 145";
+    requires.add(g);
+
+    facade.getCourseList().addCourse("CSCE 215 UNIX/Linux Fundamentals", 
+                                     "PR", 
+                                     CSCE215, 
+                                     1, 
+                                     "CSCE", 
+                                     'C', 
+                                     false, 
+                                     false, 
+                                     requires, 
+                                     "1", 
+                                     "2");
     facade.saveCourses();
   }
 
