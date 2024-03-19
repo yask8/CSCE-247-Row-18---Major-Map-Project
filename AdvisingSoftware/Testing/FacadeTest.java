458fd5af90912d7a44424f4559de06c47ed0558d
Spillmag <garrettspillman1@gmail.com>
2024-03-19 13:28:53 -0400

    added junit and testing package

--- AdvisingSoftware/Testing/FacadeTest.java
+++ AdvisingSoftware/Testing/FacadeTest.java
@@ -2,10 +2,22 @@ package AdvisingSoftware.Testing;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import AdvisingSoftware.CourseList;
+import AdvisingSoftware.MajorList;
+import AdvisingSoftware.User;
+import AdvisingSoftware.UserList;
+
 public class FacadeTest {
+    private User user = null;
+	private UserList userList = UserList.getInstance();
+    private CourseList courseList = CourseList.getInstance();
+    private MajorList majorList = MajorList.getInstance();
+
     
 }
