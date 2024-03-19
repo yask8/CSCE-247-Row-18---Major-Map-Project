cbacedcd0f94c703866d75e39705ccd2914f0138
Spillmag <garrettspillman1@gmail.com>
2024-03-19 12:56:08 -0400

    added junit and testing package

---
+++ AdvisingSoftware/Testing/FacadeTest.java
@@ -0,0 +1,11 @@
+package AdvisingSoftware.Testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class FacadeTest {
+    
+}
