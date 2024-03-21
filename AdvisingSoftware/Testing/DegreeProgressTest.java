d15f93e9ef7db2b7aec3ced802524d76c7dc22af
unknown <liaz@email.sc.edu>
2024-03-21 02:14:47 -0400

    setting up tests for CoursePlanner, DegreeProgress, and Facade

--- AdvisingSoftware/Testing/DegreeProgressTest.java
+++ AdvisingSoftware/Testing/DegreeProgressTest.java
@@ -34,8 +34,51 @@ public class DegreeProgressTest {
   @AfterEach
   public void tearDown() {}
   // Testing displayProgress method
+  /**
+   * Test displayProgress method:
+   * Set up a MajorMap object with predefined semesters and credit hours.
+   * Set up a list of completed courses with Grades objects containing course names and grades.
+   * Call the displayProgress method with the MajorMap, completed courses, and a list of Course objects.
+   * Verify that the output string contains the correct major, progress percentage, and other relevant information.
+   */
   // Testing saveCompleteCourses method
+  /**
+   * Test saveCompleteCourses method:
+   * Create an instance of DegreeProgress with an empty list of completed courses.
+   * Create a list of Grades objects representing completed courses.
+   * Call the saveCompleteCourses method with the list of completed courses.
+   * Verify that the completed courses are correctly added to the list of completed courses.
+   */
   // Testing updateCourseCompletion method
+  /**
+   * Test updateCourseCompletion method:
+   * Create an instance of DegreeProgress with some completed and incomplete courses.
+   * Create a list of Grades objects representing completed courses.
+   * Call the updateCourseCompletion method with the list of completed courses.
+   * Verify that the completed courses are moved from the incomplete courses list to the completed courses list.
+   */
   // Testing populateIncompleteCoursesFromMajorMap method
+  /**
+   * Test populateIncompleteCoursesFromMajorMap method:
+   * Create an instance of DegreeProgress.
+   * Create a MajorMap object with predefined courses for the same major as the DegreeProgress.
+   * Call the populateIncompleteCoursesFromMajorMap method.
+   * Verify that the incomplete courses list is populated with the courses from the major map that are not already completed.
+   **/
+
+  // Testing populateIncompleteCoursesFromAppArea
+  /** Test populateIncompleteCoursesFromAppArea method:
+   * Create an instance of DegreeProgress.
+   * Create an AppArea object with predefined courses.
+   * Call the populateIncompleteCoursesFromAppArea method.
+   * Verify that the incomplete courses list is populated with the courses from the application area that are not already completed.
+   */
   // Testing calculateGPA method
+  /**
+   * Test calculateGPA method:
+   * Set up a list of completed courses with Grades objects containing course names and grades.
+   * Set up a list of Course objects representing all courses.
+   * Call the calculateGPA method with the list of completed courses and the list of all courses.
+   * Verify that it returns the correct GPA.
+   */
 }
