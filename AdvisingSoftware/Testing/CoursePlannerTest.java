3ec3cf0f7496c0c6e6d4428e22afd8f032aa445e
unknown <liaz@email.sc.edu>
2024-03-21 01:54:01 -0400

    setting up tests for CoursePlanner, DegreeProgress, and Facade

--- AdvisingSoftware/Testing/CoursePlannerTest.java
+++ AdvisingSoftware/Testing/CoursePlannerTest.java
@@ -9,6 +9,7 @@ import AdvisingSoftware.Course;
 import AdvisingSoftware.CourseList;
 import AdvisingSoftware.Grades;
 import AdvisingSoftware.MajorList;
+import AdvisingSoftware.Student;
 import AdvisingSoftware.UserList;
 import java.util.ArrayList;
 import java.util.UUID;
@@ -17,6 +18,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CoursePlannerTest {
+
+  private Student student;
 
   /**
    * Methods to Test
@@ -31,8 +34,35 @@ public class CoursePlannerTest {
 
   @AfterEach
   public void tearDown() {}
+
   // Testing removeCourse method
+  @Test
+  public void testRemoveCourseMethodValid() {}
+
+  @Test
+  public void testRemoveCourseMethodSemester0() {}
+
+  @Test
+  public void testRemoveCourseMethodSemester9() {}
+
+  @Test
+  public void testRemoveCourseMethodCourseNotExist() {}
+
   // Testing addCourse method
+  @Test
+  public void testAddCourseMethodValid() {}
+
+  @Test
+  public void testAddCourseMethodSemester0() {}
+
+  @Test
+  public void testAddCourseMethodSemester9() {}
+
+  @Test
+  public void testAddCourseMethodCourseNotExist() {}
+
+  @Test
+  public void testAddCourseMethodCourseExist() {}
   // Testing generateFromMajorMap method
   // Testing searchPlanner method
 }
