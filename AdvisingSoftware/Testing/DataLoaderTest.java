f4d1512560bfb2c3a8a3ed2ad2d25034dcdabefd
yask8 <yasminekennedy88@gmail.com>
2024-03-22 01:45:54 -0400

    adding tests to DataLoaderTest and DataWriterTests

--- AdvisingSoftware/Testing/DataLoaderTest.java
+++ AdvisingSoftware/Testing/DataLoaderTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import AdvisingSoftware.DataLoader;
+import AdvisingSoftware.DataWriter;
+import AdvisingSoftware.User;
+import AdvisingSoftware.UserList;
 
 import java.io.FileReader;
 import java.text.SimpleDateFormat;
@@ -18,17 +21,79 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+/**
+ * @author Yasmine Kennedy
+ */
 public class DataLoaderTest {
+
+    private UserList userList;
+    private ArrayList<User> users;
+
     /**
-     * Methods to Test
-     * loadUsers()
-     * loadStudents()
-     * loadDegreeProgressFromJSON()
-     * loadCoursePlannerFromJSON()
-     * loadAdmin()
-     * loadAdvisors()
-     * loadCourses()
-     * loadMajors()
-     * loadStringsFromJSONArray()
+     * @author Yasmine Kennedy
      */
+    @BeforeEach
+    public void setUp() {
+        userList = UserList.getInstance();
+        users = userList.getUsers();
+
+        users.clear();
+
+        DataWriter.saveUsers(users);
+    }
+
+    /**
+     * @author Yasmine Kennedy
+     */
+    @AfterEach
+    public void tearDown(){
+        users.clear();
+
+        DataWriter.saveUsers(users);
+    }
+
+    /**
+     * @author Yasmine Kennedy
+     */
+    @Test
+    void testUsersSize() {
+        users.add(new User("Owew", "Shumate", "oshumate@email.sc.edu", "oooriley$4", "Admin"));
+        users.add(new User("Portia", "Plante", "pplante@email.sc.edu", "luvocding340", "Advisor"));
+        users.add(new User("Micheal", "Jackson", "bigmike@email.sc.edu", "neverlandsucks9", "Student"));
+        assertEquals(3, users.size());
+    }
+
+    /**
+     * @author Yasmine Kennedy
+     */
+    @Test
+    void testUsersSizeZero() {
+        assertEquals(0, users.size());
+    }
+
+    /**
+     * @author Yasmine Kennedy
+     */
+    @Test
+    void testUserFirstUserName() {
+        users.add(new User("Owen", "Shumate", "oshumate@email.sc.edu", "oooriley$4", "Admin"));
+        users.add(new User("Portia", "Plante", "pplante@email.sc.edu", "luvocding340", "Advisor"));
+        users.add(new User("Micheal", "Jackson", "bigmike@email.sc.edu", "neverlandsucks9", "Student"));
+        assertEquals("Owen", users.get(0).getFirstName());
+    }
+
+    /**
+     * @author Yasmine Kennedy
+     */
+    @Test
+    void testDuplicateUsers() {
+        users.add(new User("Owen", "Shumate", "oshumate@email.sc.edu", "oooriley$4", "Admin"));
+        users.add(new User("Portia", "Plante", "pplante@email.sc.edu", "luvocding340", "Advisor"));
+        users.add(new User("Micheal", "Jackson", "bigmike@email.sc.edu", "neverlandsucks9", "Student"));
+        users.add(new User("Owen", "Shumate", "oshumate@email.sc.edu", "oooriley$4", "Admin"));
+        assertEquals("oshumate@email.sc.edu", users.get(0).getEmail());
+        assertEquals("User already exists", users.get(3).getEmail());
+    }
+
+  
 }
--- AdvisingSoftware/Testing/DataWriterTest.java
+++ AdvisingSoftware/Testing/DataWriterTest.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.annotation.Testable;
 
 import AdvisingSoftware.DataWriter;
 import AdvisingSoftware.User;
@@ -26,11 +27,18 @@ import java.util.UUID;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
-
+/**
+ * Tester class for DataWriter
+ * @author Yasmine Kennedy (yas8) and Garrett Spillman (Spillmag)
+ */
 public class DataWriterTest {
+     
      private UserList userList;
      private ArrayList<User> users;
-
+     
+     /**
+      * @author Yasmine Kennedy
+      */
      @BeforeEach
      public void setUp() {
           userList = UserList.getInstance();
@@ -41,6 +49,9 @@ public class DataWriterTest {
           DataWriter.saveUsers(users);
      }
 
+     /**
+      * @author Yasmine Kennedy
+      */
      @AfterEach
      public void tearDown() {
 
@@ -49,11 +60,26 @@ public class DataWriterTest {
           DataWriter.saveUsers(users);
      }
 
+     /**
+      * @author Yasmine Kennedy
+      */
      @Test
      void testWritingZeroUsers() {
          assertEquals(0, users.size());
      }    
      
+      /**
+      * @author Yasmine Kennedy
+      */
+     @Test
+     void testWritingOneUser() {
+          users.add(new User("Bobby", "Jones", "bigBob@gmail.com", "Westy@1#", "Student"));
+          assertEquals(1, users.size());
+     }
+
+      /**
+      * @author Garrett Spillman
+      */
      @Test
      void testWritingTwoUsers() {
           
@@ -62,4 +88,57 @@ public class DataWriterTest {
       
           assertEquals(2, users.size());
       } 
+
+     /**
+      * @author Yasmine Kennedy
+      */
+      @Test
+      void testWritingNullUser(){
+          users.add(new User(null, "", "", "", ""));
+          assertEquals(null, users.get(0).getFirstName());
+      }
+
+     /**
+      * @author Yasmine Kennedy
+      */
+      @Test
+      void testWritingNoDuplicateUsers() {
+          users.add(new User("Sam", "Smith", "ss@email.sc.edu", "hithere", "Student"));
+          users.add(new User("Sam", "Smith", "ss@email.sc.edu", "hithere", "Student"));
+          assertEquals("ss@email.sc.edu", users.get(0).getEmail());
+          assertEquals("User already exists", users.get(1).getEmail());
+      }
+      
+     /**
+      * @author Yasmine Kennedy
+      */
+      @Test
+      void testWritingNoDuplicateEmailWithDifferentUserTypes() {
+          users.add(new User("Rain", "Webster", "rw@email.sc.edu", "lostinsauce@3", "Advisor"));
+          users.add(new User("Ross", "Williams", "rw@email.sc.edu", "wafflesisgood@4", "Admin"));
+          assertEquals("rw@email.sc.edu", users.get(0).getEmail());
+          assertEquals("Email already exists", users.get(1).getEmail());
+      }
+
+     /**
+      * @author Yasmine Kennedy
+      */
+      @Test
+      void testWritingEmptyUser() {
+          users.add(new User("", "", "", null, ""));
+          assertEquals("", users.get(0).getFirstName());
+      }
+
+     /**
+      * @author Yasmine Kennedy
+      */
+      @Test
+      void testWritingDuplicateLastNamesUsers() {
+          users.add(new User("Tom", "Holland", "tholland@email.sc.edu", "spidermanrules#", "Advisor"));
+          users.add(new User("James", "Holland", "jamesh@email.sc.edu", "123hagotyou", "Advisor"));
+          assertEquals("Tom", users.get(0).getFirstName());
+          assertEquals("Holland", users.get(0).getLastName());
+          assertEquals("James", users.get(1).getFirstName());
+          assertEquals("Holland", users.get(1).getLastName());
+      }
 }
