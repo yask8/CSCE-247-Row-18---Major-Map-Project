e53e2cb6326790e542d04977208ee8d31adff6a8
unknown <liaz@email.sc.edu>
2024-03-22 21:33:29 -0400

    setting up StudentTest, CourseTest, and CourseListTest

--- AdvisingSoftware/Testing/CourseTest.java
+++ AdvisingSoftware/Testing/CourseTest.java
@@ -2,9 +2,7 @@ package AdvisingSoftware.Testing;
 
 public class CourseTest {
   /**
+   * Methods to Test (following methods are empty)
    * + editCourse(String id): Course
-   * + deleteCourse(): void
-   * + isElective(): boolean
-   * + isCarolinaCore(): boolean
    */
 }
